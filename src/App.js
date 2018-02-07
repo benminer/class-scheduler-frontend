84b23b8cb5894375d6fc36e0fbfe1e50a111aa3b
sebastianalegre <salegre.morris@gmail.com>
2018-02-06 18:50:42 -0600

    improved header and added alert bar

--- src/App.js
+++ src/App.js
@@ -43,38 +43,42 @@ class App extends Component {
     // }).then(response => console.log(response.data.allCourses))
   }
 
-//   renderClasses({ data: { courses, refetch } }) {
-//   return (
-//     <div className="formButton">
-//       <Button className="button" size="lg" outline color="primary" onClick={() => refetch()}>
-//         Refresh
-//       </Button>
-//       <ul>
-//         {courses.map(course => (
-//           <li key={course.crn}>
-//             {course.title}
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
+  //   renderClasses({ data: { courses, refetch } }) {
+  //   return (
+  //     <div className="formButton">
+  //       <Button className="button" size="lg" outline color="primary" onClick={() => refetch()}>
+  //         Refresh
+  //       </Button>
+  //       <ul>
+  //         {courses.map(course => (
+  //           <li key={course.crn}>
+  //             {course.title}
+  //           </li>
+  //         ))}
+  //       </ul>
+  //     </div>
+  //   );
+  // }
 
   render() {
     return (
       <div className="App">
-      <div className="AppContent">
-        <Image src={Images.belmont} as='img' className="App-logo"/>
-        <Header as='h1' className="App-header">Class Scheduler </Header>
-      </div>
-      <div className="Content">
-        <Divider as='hr' className="Divider" horizontal/>
-        <Header as='h4' className="SearchText"> Search for a class... </Header>
-        <div className="SearchContainer">
-          <Search className="Search" />
+        <div className="AppContent">
+          <div class="row">
+            <div class="left"><Image src={Images.belmont} as='img' className="App-logo"/></div>
+            <div class="column"><Header as='h1' className="App-header">Class Scheduler </Header></div>
+            <div class="right"><Image src={Images.belmont} as='img' className="App-logo-l"/></div>
+          </div>
+        </div>
+        <div className="Content">
+          <Divider as='hr' className="Divider" horizontal/>
+          <p className="App-text">Important alert! Monkeys will die upon exposure to book-worms</p>
+          <Header as='h4' className="SearchText"> Search for a class... </Header>
+          <div className="SearchContainer">
+            <Search className="Search" />
+          </div>
         </div>
       </div>
-    </div>
     );
   }
 }
